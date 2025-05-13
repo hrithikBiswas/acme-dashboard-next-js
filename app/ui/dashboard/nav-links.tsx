e08ea0a002ea80fa5176c9506e18d4c38e2349e4
Hrithik Biswas <mr.hrithikbiswas@gmail.com>
2025-05-13 15:54:15 +0600

    feat: implement database seeding endpoints and update seed logic for users, customers, invoices, and revenue

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -7,7 +7,6 @@ import {
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
-import { log } from 'console';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
